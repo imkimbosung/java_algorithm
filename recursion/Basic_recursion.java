ccf47d7d3b046df7f8d7ca187e94e74d955ae900
Imkimbosung <ghalsru@naver.com>
2021-03-30 14:11:57 +0900

    basic resurcion recommit

---
+++ recursion/Basic_recursion.java
@@ -0,0 +1,24 @@
+/*
+ * 21.03.28
+ * 문제 : 함수를 사용해 1 ~ 100까지 숫자 중 홀수의 합을 구해라
+ */
+public class Main {
+   
+   public static void main(String args[]) {
+      System.out.println(recursionSum(1));
+   }
+   
+   public static int recursionSum(int startNum) {
+      
+      // 마지막 정의
+      if(startNum == 100) {
+         return 0;
+      
+      // 홀수일때만 합산   
+      }else if(startNum % 2 == 1) {
+         return startNum + recursionSum(++startNum);
+      }
+      // 짝수일때는 합산하지 않음
+      return  recursionSum(++startNum);
+   }
+}
