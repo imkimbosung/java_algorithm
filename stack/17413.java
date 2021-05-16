14356c6fa34d16fd4c4d4a9b87f1da070235cd3d
imkimbosung <ghalsru@naver.com>
2021-05-16 18:20:14 +0900

    baekjoon 17413번

---
+++ stack/17413.java
@@ -0,0 +1,55 @@
+/*
+baekjoon 17413번
+문자열 S가 주어졌을 때, 이 문자열에서 단어만 뒤집으려고 한다.
+
+먼저, 문자열 S는 아래와과 같은 규칙을 지킨다.
+
+    알파벳 소문자('a'-'z'), 숫자('0'-'9'), 공백(' '), 특수 문자('<', '>')로만 이루어져 있다.
+    문자열의 시작과 끝은 공백이 아니다.
+    '<'와 '>'가 문자열에 있는 경우 번갈아가면서 등장하며, '<'이 먼저 등장한다. 또, 두 문자의 개수는 같다.
+    태그는 '<'로 시작해서 '>'로 끝나는 길이가 3 이상인 부분 문자열이고, 
+    '<'와 '>' 사이에는 알파벳 소문자와 공백만 있다. 단어는 알파벳 소문자와 숫자로 이루어진 부분 문자열이고, 
+    연속하는 두 단어는 공백 하나로 구분한다. 태그는 단어가 아니며, 태그와 단어 사이에는 공백이 없다.
+*/
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String args[]) throws IOException {
+		Scanner input = new Scanner(System.in);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Stack<Character> stack = new Stack<>();
+		
+		String inputLine = input.nextLine() + "\n";
+		boolean findTag = false;
+		for(char str : inputLine.toCharArray()) {
+			
+			if(str == '<') {
+				if(!stack.empty()) {
+					while(!stack.empty()) {
+						bw.write(stack.pop());
+					}
+				}
+				findTag = true;
+				bw.write(str);
+			}else if(str == '>'){
+				findTag = false;
+				bw.write(str);
+			}else if(findTag) {
+				bw.write(str);
+			}else if(str == ' ' || str == '\n') {
+				while(!stack.empty()) {
+					bw.write(stack.pop());
+				}
+				bw.write(str);
+			}else {
+				stack.push(str);
+			}
+		}
+		
+		bw.flush();
+		
+	}
+}
+
