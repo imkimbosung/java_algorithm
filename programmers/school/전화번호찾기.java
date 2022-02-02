7f3723bbbd22e88134d8878b8069eee26b77c49a
KimBoSung <ghalsru@naver.com>
2022-02-02 17:52:21 +0900

    2번째 풀이
    2번을 사용하면 속도가 나오지 않아 반복문을 1번만 사용하는 것이 포인트였다.

--- programmers/school/전화번호찾기.java
+++ programmers/school/전화번호찾기.java
@@ -3,22 +3,38 @@
  * 
  * 현재버전 : 정확성 테스트 완료
  *          효율성 테스트 2개 실패
+ * 
+ * 두번째 버전 : 정확성, 효율성 통과
+ *          Arrays.sort()를 통해 앞이 비슷한 부분끼리 정렬 후 for문을 한번만 사용하여 문제를 푼게 포인트였다.
+ *          indexOf 대신 startsWith를 사용해도 된다.
  */
+import java.util.Arrays;
 
 public class 전화번호찾기 {
 
+    // public boolean solution(String[] phone_book) {
+    //     for(int i = 0 ; i < phone_book.length ; i++){
+    //         for(int j = 0 ; j < phone_book.length ; j++){
+    //             if(i == j){
+    //                 continue;
+    //             }
+    //             if(phone_book[j].indexOf(phone_book[i]) == 0){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+
     public boolean solution(String[] phone_book) {
-        for(int i = 0 ; i < phone_book.length ; i++){
-            for(int j = 0 ; j < phone_book.length ; j++){
-                if(i == j){
-                    continue;
-                }
-                if(phone_book[j].indexOf(phone_book[i]) == 0){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+        Arrays.sort(phone_book);
+
+       for(int i = 0 ; i < phone_book.length-1 ; i++){
+           if(phone_book[i+1].indexOf(phone_book[i]) == 0){
+               return false;
+           }
+       }
+       return true;
+   }
     
 }
