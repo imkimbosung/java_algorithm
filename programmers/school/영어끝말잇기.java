968eeaa53fb285d630123620c4fc463246e0efd3
KimBoSung <ghalsru@naver.com>
2022-02-07 23:13:21 +0900

    Update 영어끝말잇기.java
    indexof를 사용하여 중복을 거르는 부분에서 오류가 있었다. 앞으로는 Set 자료구조를 이용하여 중복을 확실하게 걸러보자

--- programmers/school/영어끝말잇기.java
+++ programmers/school/영어끝말잇기.java
@@ -1,15 +1,19 @@
 /*
 95점 받은 코드 .. 나머지 5점을 위해 수정 시작
+100점 받은 코드 .. "중복"이라는 단어가 나오면 set자료구조를 떠올리자.
 */
+import java.util.*;
 
 public class 영어끝말잇기 {
     public int[] solution(int n, String[] words) {
         String tmpString = words[0];
         int per = 0;
         int num = 0;
+        HashMap<String,Integer> tmpMap = new HashMap<>();
+        tmpMap.put(tmpString, 1);
         for(int i = 1 ; i < words.length; i++){
-            if(tmpString.indexOf(words[i]) < 0 
-                && tmpString.charAt(tmpString.length()-1) == words[i].charAt(0)){
+            if(tmpMap.getOrDefault(words[i], -1) == -1 && words[i].charAt(0) == tmpString.charAt(tmpString.length()-1)){
+                tmpMap.put(words[i], 1);
                 tmpString += " " + words[i];
                 continue;
             }
@@ -21,3 +25,5 @@ public class 영어끝말잇기 {
         return new int[] {per, num};
     }
 }
+
+
