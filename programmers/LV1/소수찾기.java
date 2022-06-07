c91443de455ba6106164f63d5a08ce002ba56a4b
KimBoSung <ghalsru@naver.com>
2022-06-07 23:29:58 +0900

    Update 소수찾기.java
    2번 풀이 약수와 루트로 더 빠른풀이

--- programmers/LV1/소수찾기.java
+++ programmers/LV1/소수찾기.java
@@ -1,18 +1,19 @@
 public class 소수찾기 {
+    
+    public int solution(int n) {
+		int answer = 0;
+		for(int i = 2 ; i <= n ; i++){
+			if(checkPrimeNum2(i)){answer++;}
+		}
+        return answer;
+    }
+    
     /**
      *  1. 번 풀이
      *      N 숫자 절반까지만 체크하는 로직
      *      예) 12면 12/2 = 6 이므로 2~6 까지만 확인해보면 된다.
      */
-    public int solution(int n) {
-		int answer = 0;
-		for(int i = 2 ; i <= n ; i++){
-			if(checkPrimeNum(i)){answer++;}
-		}
-        return answer;
-    }
-    
-    private boolean checkPrimeNum(int n){
+    private boolean checkPrimeNum1(int n){
 		for(int j = 2 ; j <= n/2 ; j++){
 			if(n%j == 0){
 				return false;
@@ -20,4 +21,19 @@ public class 소수찾기 {
 		}
 		return true;
 	}
+
+    /**
+     *  2. 번 풀이
+     *      소수는 결론적으로 특정N값의 약수값들이 1과 자기 자신을 제외하면 없어야 한다. 그 중앙값인 루트 N으로 비교해보면 된다. 
+     *      예) 12면 루트12 이므로 2~3.5 까지만(자연수만 비교하므로 3까지) 확인해보면 된다. 
+     */
+    private boolean checkPrimeNum2(int n){
+		for(int j = 2 ; j*j <= n; j++){
+			if(n%j == 0){
+				return false;
+			}
+		}
+		return true;
+	}
+    
 }
