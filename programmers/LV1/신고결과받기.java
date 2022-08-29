1ac4cba95326abc08d9ec9c9605231dd4733a984
KimBoSung <ghalsru@naver.com>
2022-08-29 23:26:53 +0900

    Update 신고결과받기.java
    성공한 코드로 교체

--- programmers/LV1/신고결과받기.java
+++ programmers/LV1/신고결과받기.java
@@ -1,54 +1,35 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
+        Map<String, Integer> reportMap = new HashMap<>();
+        Map<String, Integer> idMap = new HashMap<>();
+        report = Arrays.stream(report).distinct().toArray(String[]::new);
         
-        Map<String, Integer> reportCntMap = new HashMap<>();
-        Map<String, String> reportUsrMap = new HashMap<>();
+        for(String reportVal : report){
+            String splitSt = reportVal.split(" ")[1];
+            reportMap.put(splitSt, reportMap.getOrDefault(splitSt, 0) + 1);
+        }
         
-        String checkValue = "";
-        
-        for(String reportValue : report){
+        for(String reportVal : report){
+            String reportId = reportVal.split(" ")[1];
+            int reportCnt = reportMap.get(reportId);
             
-            // 한 유저가 같은 유저를 2회 이상 신고하지 못하도록 검증 로직
-            if(checkValue.indexOf(reportValue) < 0){
-                String key = reportValue.split(" ")[0];
-                String value = reportValue.split(" ")[1];
-                // 유저가 리폿 당한 횟수
-                reportCntMap.put(value, reportCntMap.getOrDefault(value, 0)+1);
-                // 신고한 유저    
-                reportUsrMap.put(value, reportUsrMap.getOrDefault(value, "")+key);
-                
-                checkValue += reportValue + " ";
+            if(reportCnt >= k){
+                String splitSt = reportVal.split(" ")[0];
+                idMap.put(splitSt, idMap.getOrDefault(splitSt, 0) + 1);
             }
         }
         
-        Map<String, Integer> finalCntMap = getReportCntMap(id_list, reportCntMap, reportUsrMap, k);
-        
-        
-        for(int i = 0 ; i < id_list.length ; i++){
-           answer[i] = finalCntMap.getOrDefault(id_list[i], 0);
+        for(int i = 0 ; i < answer.length ; i++){
+            answer[i] = idMap.getOrDefault(id_list[i], 0);
         }
         
         
         return answer;
     }
-    
-    private Map<String, Integer> getReportCntMap(String[] id_list, Map<String, Integer> reportCntMap, Map<String, String> reportUsrMap, int k){
-        Map<String, Integer> returnCntMap = new HashMap<>();
-        
-        for(String tmpUsr : id_list){
-            if(reportCntMap.getOrDefault(tmpUsr, 0) >= k){
-                for(String findUsr : id_list){
-                    int reportCnt = returnCntMap.getOrDefault(findUsr, 0);
-                    reportCnt = reportUsrMap.get(tmpUsr).indexOf(findUsr) >= 0 ? ++reportCnt : reportCnt;
-                    returnCntMap.put(findUsr, reportCnt);
-                }
-            }
-        }
-        
-        return returnCntMap;
-    }
+   
 }
